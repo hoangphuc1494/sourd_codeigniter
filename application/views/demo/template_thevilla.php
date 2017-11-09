df390b7f0b96eb023c5deb8125e98039626dfad6
BaoViet <vietbeto@gmail.com>
2017-11-09 17:37:50 +0700

    phuc 11/9/2017

--- application/views/demo/template_thevilla.php
+++ application/views/demo/template_thevilla.php
@@ -6,17 +6,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="utf-8">
 	<title>The village</title>
 	<link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/css/materialize.min.css'>
-	<link rel="stylesheet" href="../../assest/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="../../assest/materialize-slider/css/style.css">
-	<style type="text/css"></style>
+	<link rel="stylesheet" href="<?php echo base_url()?>assest/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo base_url()?>assest/materialize-slider/css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
 </head>
 <body>
 	<div class="wrapper"> 
-		<header>
+		<header class="clearfix">
 			<div class="container">
 				<div class="menu-top clearfix">
 					<div class="col-md-4 col-sm-4">
-						<div class="logo"><h3>THE VILLAGE</h3></div>
+						<!-- <div class="logo"><h3>THE VILLAGE</h3></div> -->
 					</div>
 					<div class="col-md-4 col-sm-4">
 						<div class="info-header-top">
@@ -32,41 +32,210 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					</div>  
 				</div>
 			</div>
-			<div class="slider clearfix">
-				<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-					<ol class="carousel-indicators">
-						<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-						<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-						<li data-target="#carousel-example-generic" data-slide-to="2"></li>
-						<li data-target="#carousel-example-generic" data-slide-to="3"></li>
-					</ol>
-					<div class="carousel-inner">
-						<div class="item active">
-							<img src="<?php echo base_url(); ?>images/image_banner/slide1.jpg"> 
+			<section class="header clearfix">
+				<div class="container relative">
+					<div class="menu-header">
+						<div class="menu-main clearfix">
+							<span class="logo-V">
+								<img src="<?php echo base_url()?>assest/images/logo-V.png">
+							</span>
+							<ul>
+								<li><a href="#">About Us</a></li>
+								<li><a href="#">Explore</a></li>
+								<li><a href="#" class="events-link">Events</a></li>
+								<li><a href="#" class="book-link">Book now</a></li>
+							</ul>
 						</div>
-						<div class="item">
-							<img src="<?php echo base_url(); ?>images/image_banner/slide2.jpg"> 
-						</div>
-						<div class="item">
-							<img src="<?php echo base_url(); ?>images/image_banner/slide3.jpg"> 
-						</div>
-						<div class="item">
-							<img src="<?php echo base_url(); ?>images/image_banner/slide4.jpg"> 
-						</div> 
 					</div>
 				</div>
-				<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-					<span class="glyphicon glyphicon-chevron-left"></span>				</a>
-				<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-					<span class="glyphicon glyphicon-chevron-right"></span>
-				</a>
+				<div class="slider clearfix">
+					<div class="owl-carousel owl-theme">
+						<div class="item">
+							<div class="slider-header">
+								<img src="<?php echo base_url(); ?>images/image_banner/slide1.jpg" alt="Los Angeles"> 
+							</div>
+						</div>
+						<div class="item">
+							<div class="slider-header">
+								<img src="<?php echo base_url(); ?>images/image_banner/slide2.jpg" alt="Los Angeles"> 
+							</div>
+						</div> 
+						<div class="item">
+							<div class="slider-header">
+								<img src="<?php echo base_url(); ?>images/image_banner/slide3.jpg" alt="Los Angeles"> 
+							</div>
+						</div> 
+						<div class="item">
+							<div class="slider-header">
+								<img src="<?php echo base_url(); ?>images/image_banner/slide4.jpg" alt="Los Angeles"> 
+							</div>
+						</div> 
+					</div> 
+				</div>
+			</section>
+		</header>  
+		<div class="pag-header">
+			<div class="container">
+				<p><span>THE VILLAGE <i></i> DA NANG</span> Imagine ... Your own piece of paradise...</p>
 			</div>
-		</header> 
+		</div>
+		<section class="content-book">
+			<div class="container">
+				<div class="row clearfix form-group-20">
+					<div class="col-md-6 col-sm-6">
+						<div class="book-room-info">
+							<h3>Book a room</h3>
+							<p>Our ocean view bungalows make you wish you never have to leave and give you the true exclusive beach experience. Our beachfront bungalow is just as comfortable and features the same complete setup with a full bath, refrigerator, and bar.</p>
+							<a href="#">Book now >> </a>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6">
+						<div class="book-gallery clearfix row">
+							<div class="col-md-4 col-sm-4">
+								<img src="<?php echo base_url()?>assest/images/1.jpg">
+							</div>
+							<div class="col-md-4 col-sm-4">
+								<img src="<?php echo base_url()?>assest/images/1.jpg">
+							</div>
+							<div class="col-md-4 col-sm-4">
+								<img src="<?php echo base_url()?>assest/images/1.jpg">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row clearfix form-group-20">
+					<div class="col-md-6 col-sm-6">
+						<div class="book-room-info">
+							<h3>Book a room</h3>
+							<p>Our ocean view bungalows make you wish you never have to leave and give you the true exclusive beach experience. Our beachfront bungalow is just as comfortable and features the same complete setup with a full bath, refrigerator, and bar.</p>
+							<a href="#">Book now >> </a>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6">
+						<div class="book-gallery clearfix row">
+							<div class="col-md-4 col-sm-4">
+								<img src="<?php echo base_url()?>assest/images/1.jpg">
+							</div>
+							<div class="col-md-4 col-sm-4">
+								<img src="<?php echo base_url()?>assest/images/1.jpg">
+							</div>
+							<div class="col-md-4 col-sm-4">
+								<img src="<?php echo base_url()?>assest/images/1.jpg">
+							</div>
+						</div>
+					</div>
+				</div>
+				<hr>
+			</div>
+		</section>
+		<section class="content-book">
+			<div class="container"> 
+				<div class="row clearfix form-group-20">
+					<div class="col-md-6 col-sm-6 no-padding">
+						<div class="overlay-village-img">
+							<img src="<?php echo base_url()?>assest/images/2.jpg">
+							<div class="option-text">
+								<a href="#">You a #VILLAGE?</a>
+							</div>
+						</div> 
+					</div>  
+					<div class="col-md-6 col-sm-6 no-padding">
+						<div class="overlay-village-img">
+							<img src="<?php echo base_url()?>assest/images/3.jpg">
+							<div class="option-text">
+								<p> To become a vilager <a href="#">Click here</a></p>
+							</div>
+						</div> 
+					</div>  
+				</div>
+				<hr>
+			</div>
+		</section>
+		<section class="content-book">
+			<div class="container">
+				<div class="row clearfix form-group-20">
+					<div class="col-md-6 col-sm-6">
+						<div class="book-room-info">
+							<h3>Book a room</h3>
+							<p>Our ocean view bungalows make you wish you never have to leave and give you the true exclusive beach experience. Our beachfront bungalow is just as comfortable and features the same complete setup with a full bath, refrigerator, and bar.</p>
+							<a href="#">Book now >> </a>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6">
+						<div class="book-gallery clearfix row">
+							<div class="col-md-4 col-sm-4">
+								<img src="<?php echo base_url()?>assest/images/1.jpg">
+							</div>
+							<div class="col-md-4 col-sm-4">
+								<img src="<?php echo base_url()?>assest/images/1.jpg">
+							</div>
+							<div class="col-md-4 col-sm-4">
+								<img src="<?php echo base_url()?>assest/images/1.jpg">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row clearfix form-group-20">
+					<div class="col-md-6 col-sm-6">
+						<div class="book-room-info">
+							<h3>Book a room</h3>
+							<p>Our ocean view bungalows make you wish you never have to leave and give you the true exclusive beach experience. Our beachfront bungalow is just as comfortable and features the same complete setup with a full bath, refrigerator, and bar.</p>
+							<a href="#">Book now >> </a>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6">
+						<div class="book-gallery clearfix row">
+							<div class="col-md-4 col-sm-4">
+								<img src="<?php echo base_url()?>assest/images/1.jpg">
+							</div>
+							<div class="col-md-4 col-sm-4">
+								<img src="<?php echo base_url()?>assest/images/1.jpg">
+							</div>
+							<div class="col-md-4 col-sm-4">
+								<img src="<?php echo base_url()?>assest/images/1.jpg">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row clearfix form-group-20">
+					<div class="col-md-6 col-sm-6">
+						<div class="book-room-info">
+							<h3>Book a room</h3>
+							<p>Our ocean view bungalows make you wish you never have to leave and give you the true exclusive beach experience. Our beachfront bungalow is just as comfortable and features the same complete setup with a full bath, refrigerator, and bar.</p>
+							<a href="#">Book now >> </a>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6">
+						<div class="book-gallery clearfix row">
+							<div class="col-md-4 col-sm-4">
+								<img src="<?php echo base_url()?>assest/images/1.jpg">
+							</div>
+							<div class="col-md-4 col-sm-4">
+								<img src="<?php echo base_url()?>assest/images/1.jpg">
+							</div>
+							<div class="col-md-4 col-sm-4">
+								<img src="<?php echo base_url()?>assest/images/1.jpg">
+							</div>
+						</div>
+					</div>
+				</div>
+				<hr>
+			</div>
+		</section>
 	</div>
 
 	<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 	<script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/0.96.1/js/materialize.min.js'></script>
-	<script  src="../../assest/bootstrap/js/bootstrap.min.js"></script>
-	<script  src="../../assest/materialize-slider/js/index.js"></script>
+	<script  src="<?php echo base_url()?>assest/bootstrap/js/bootstrap.min.js"></script>
+	<script  src="<?php echo base_url()?>assest/materialize-slider/js/owl.carousel.js"></script>
+	<script  src="<?php echo base_url()?>assest/materialize-slider/js/index.js"></script>
+	<script>
+		$('.owl-carousel').owlCarousel({
+			loop:true,
+			margin:10,
+			items: 1,
+			nav:true 
+		})
+	</script>
 </body>
 </html>
